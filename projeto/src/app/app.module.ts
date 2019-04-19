86981da8169f5da4ddeddb420b1f10f423b83156
Douglas Fernandes <douglasfpdasilvajunior@gmail.com>
2019-04-19 19:42:35 -0300

    separar alteraçoes para outra branch 2

--- projeto/src/app/app.module.ts
+++ projeto/src/app/app.module.ts
@@ -4,7 +4,6 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BoloListComponent } from './bolo/bolo-list/bolo-list.component';
-import { PaisesModule } from './paises/paises.module';
 
 @NgModule({
   declarations: [

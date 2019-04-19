4759232c957b88153ef6898aea9da508864adcca
Douglas Fernandes <douglasfpdasilvajunior@gmail.com>
2019-04-19 16:15:48 -0300

    adicionar boloList

---
+++ projeto/src/app/bolo/bolo-list/bolo-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bolo-list',
+  templateUrl: './bolo-list.component.html',
+  styleUrls: ['./bolo-list.component.css']
+})
+export class BoloListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

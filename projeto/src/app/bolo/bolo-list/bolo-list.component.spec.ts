c635d51c494c8050b4b3d3736312706666fe8ef4
Douglas Fernandes Pereira da Silva Jr <douglas.pereira@iteris.com.br>
2019-12-18 18:08:20 -0300

    adicionar exemplos de validação de eventos

--- projeto/src/app/bolo/bolo-list/bolo-list.component.spec.ts
+++ projeto/src/app/bolo/bolo-list/bolo-list.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed, tick, fakeAsync } from '@angular/core/testing';
 
 import { BoloListComponent } from './bolo-list.component';
 import { FormsModule } from '@angular/forms';
@@ -9,10 +9,10 @@ describe('BoloListComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ BoloListComponent ],
-      imports:[FormsModule]
+      declarations: [BoloListComponent],
+      imports: [FormsModule]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
@@ -24,4 +24,48 @@ describe('BoloListComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should start enabled', () => {
+    const compiled = fixture.debugElement.nativeElement as HTMLElement;
+    const button = compiled.querySelector('button') as HTMLButtonElement;
+    const txt = compiled.querySelector('input[type=text]') as HTMLInputElement;
+
+    expect(button.disabled).toBeFalsy();
+    expect(txt.disabled).toBeFalsy();
+  });
+
+  it('should Add', fakeAsync(() => {
+    const compiled = fixture.debugElement.nativeElement as HTMLElement;
+    const button = compiled.querySelector('button') as HTMLButtonElement;
+    const txt = compiled.querySelector('input[type=text]') as HTMLInputElement;
+
+    txt.value = 'NovoValor';
+    txt.dispatchEvent(new Event('input'));
+    fixture.detectChanges();
+    tick();
+    button.click();
+    fixture.detectChanges();
+    tick();
+    const listaLi = compiled.querySelectorAll('li').length;
+    expect(listaLi).toEqual(4);
+
+  }));
+
+  it('should disable', async () => {
+    const compiled = fixture.debugElement.nativeElement as HTMLElement;
+    const checkbox = compiled.querySelector('input[type=checkbox]') as HTMLInputElement;
+    checkbox.click();
+    fixture.detectChanges();
+    fixture.whenStable().then(() => {
+      const button = compiled.querySelector('button') as HTMLButtonElement;
+      const txt = compiled.querySelector('input[type=text]') as HTMLInputElement;
+      const h3 = compiled.querySelector('h3') as HTMLElement;
+
+      expect(button.disabled).toBeTruthy();
+      expect(txt.disabled).toBeTruthy();
+      expect(h3).not.toBeNull();
+    });
+  });
+
+
 });

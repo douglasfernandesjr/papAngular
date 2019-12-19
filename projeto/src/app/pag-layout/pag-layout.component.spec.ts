f0af268dfc3a29c9f9478993079869b8e70696b9
douglasfernandesjr <douglasfpdasilvajunior@gmail.com>
2019-12-19 11:16:53 -0300

    atualizações

--- projeto/src/app/pag-layout/pag-layout.component.spec.ts
+++ projeto/src/app/pag-layout/pag-layout.component.spec.ts
@@ -1,5 +1,4 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { PagLayoutComponent } from './pag-layout.component';
 import { RouterTestingModule } from '@angular/router/testing';
 
